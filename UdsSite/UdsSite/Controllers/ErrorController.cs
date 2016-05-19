1acc198b37dd88762b8cd089aa1d570f9f0093c9
Igor Pyanzin <igor.pyanzin@gmail.com>
2016-05-19 15:25:18 +0300

    MVC skeleton with some standalone frontpage

---
+++ UdsSite/UdsSite/Controllers/ErrorController.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Mvc;
+
+namespace UdsSite.Controllers {
+    public class ErrorController : Controller {
+        public ActionResult Http404() {
+            Response.StatusCode = 404;
+
+            return View();
+        }
+    }
+}
